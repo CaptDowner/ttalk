0625a8f92d27ce87aac6cde9916796f0c33b32b9
Capt. Downer <captdowner@gmail.com>
2015-02-11 19:35:24 -0800

    Added private method devise_parameter_sanitizer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:sign_up) << :password
+    devise_parameter_sanitizer.for(:sign_up) << :password_confirmation
+  end
 end
