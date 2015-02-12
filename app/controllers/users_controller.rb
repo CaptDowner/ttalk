cbad9f90b80d69468cbe4aab4736e0ac11628b48
Capt. Downer <captdowner@gmail.com>
2015-02-11 19:52:14 -0800

    Added users controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  after_action :verify_authorized
+
+  def index
+    @users = User.all
+    authorize User
+  end
+
+  def initialize
+   @user.mode = :speak  # speak mode
+   @user.voice = :male  # male voice
+  end
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    authorize user
+    user.destroy
+    redirect_to users_path, :notice => "User deleted."
+  end
+
+  def mode
+
+  private
+
+  def secure_params
+    params.require(:user).permit(:role)
+  end
+
+
+e callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:user_id, :mode, :voice)
+    end
+
+END
