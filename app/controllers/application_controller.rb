aa906701bb60214ebbab7e2627449e300c4d8f46
Capt. Downer <captdowner@gmail.com>
2015-02-14 11:08:22 -0800

    Added private section for Application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
+ # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+   
+  private
+ 
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
