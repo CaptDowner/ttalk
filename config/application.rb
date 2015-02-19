6ca3b553f2913d8982a0aa90406beaec3e0940c5
Capt. Downer <captdowner@gmail.com>
2015-02-19 13:26:31 -0800

    Changed default locale back to en.

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,11 @@ module RailsDevisePundit
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+#   config.assets.paths << "#{Rails.root}/app/assets/sounds"
+#    config.assets.paths << Rails.root.join("app", "assets", "sounds")
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ar
+    config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
