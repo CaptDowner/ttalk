512de438a1aaf28d2271b2fb143fdfd38d2f6c70
Capt. Downer <captdowner@gmail.com>
2015-02-20 12:42:28 -0800

    Added options table to db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216231111) do
+ActiveRecord::Schema.define(version: 20150220153950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150216231111) do
   create_table "languages", force: :cascade do |t|
     t.string   "name"
     t.string   "abbrv"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "options", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "voice"
+    t.string   "lang_abbrv"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
