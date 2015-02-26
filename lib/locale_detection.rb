e067af16e5eb877c0700f005fcd16b5dc595f7d8
Capt. Downer <captdowner@gmail.com>
2015-02-26 12:37:14 -0800

    Added local_detection module.

---
+++ lib/locale_detection.rb
@@ -0,0 +1,27 @@
+module LocaleDetection
+  protected
+
+ def switch_locale
+    locale = params[:locale] || cookies[:locale]
+    if allowed_locale?(locale)
+      I18n.locale = locale
+    else
+      I18n.locale = default_locale
+    end
+    cookies[:locale] = {
+      value:   locale,
+      expires: 1.year.from_now,
+      domain:  request.domain
+    }
+  end
+
+  def allowed_locale?(locale)
+    I18n.available_locales.include?(:"#{locale}")
+  end
+
+  def default_locale
+    I18n.locale || I18n.default_locale
+#    Whitelabel.label ? Whitelabel[:default_locale] : I18n.default_locale
+  end
+end
+
