d27c72f2bd9f0dd5d80a4fdd79b8d1bf12cfb7c3
Capt. Downer <captdowner@gmail.com>
2015-02-28 10:14:44 -0800

    Renamed app from Rails to TommyTalker.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsDevisePundit
+module TommyTalker
   class Application < Rails::Application
 
     config.generators do |g|
