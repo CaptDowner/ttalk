7a9604bd6fd910ae3c61b93e04e30fd3d3f23b7e
Capt. Downer <captdowner@gmail.com>
2015-03-01 15:03:44 -0800

    Added a show_flash method to fade out some flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def show_flash(options={})
+    options = {:fade => 1, :display => 2, :highlight => 3}.merge(options)
+    html = content_tag(:div, flash.collect{ |key,msg| content_tag(:div, msg, :class => key, :attributes => "style = display: none;") }, :id => 'flash-message')
+    html << content_tag(:script, "new Effect.Highlight('flash-message', {duration: #{options[:highlight]}});") if options[:highlight]
+    html << content_tag(:script, "setTimeout(\"$('flash-message').fade({duration: #{options[:fade]}});\", #{options[:display]*1000}); return false;")
+  end
 end
