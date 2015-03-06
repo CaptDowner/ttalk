270803fd7090cdfa78664f524bdb4556411f22d9
Capt. Downer <captdowner@gmail.com>
2015-03-05 17:32:06 -0800

    Removed column abbrev from phrases table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222221053) do
+ActiveRecord::Schema.define(version: 20150305160916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20150222221053) do
     t.integer  "language_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "abbrev"
     t.string   "key"
   end
 
