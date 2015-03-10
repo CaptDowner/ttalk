9afc66b6fdd084e5fb89ee9089dfa6fea4b6f693
Capt. Downer <captdowner@gmail.com>
2015-03-09 19:49:07 -0700

    Added has_and_belongs_to_many relationships.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :category_phrases
-  has_many :phrases, through: :category_phrases
+  # http://www.ruby-journal.com/rails-4-changes-join-table-naming-convention/
+  has_and_belongs_to_many :phrases, join_table: :category_phrases
+#  has_many :phrases, through: :category_phrases
   has_and_belongs_to_many :users
 end
--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -2,5 +2,7 @@ class Phrase < ActiveRecord::Base
   paginates_per 30
   belongs_to :language
   has_many :category_phrases
-  has_many :categories, through: :category_phrases
+  # http://www.ruby-journal.com/rails-4-changes-join-table-naming-convention/
+  has_and_belongs_to_many :categories, join_table: :category_phrases
+#  has_many :categories, through: :category_phrases
 end
