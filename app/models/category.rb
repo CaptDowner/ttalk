21c15857a5387231c156f4a1fef036ef15b86c47
Capt. Downer <captdowner@gmail.com>
2015-03-13 12:36:01 -0700

    Added populate_by_phrase_key(phrase_key) method.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,12 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :phrases, join_table: :category_phrases
 #  has_many :phrases, through: :category_phrases
   has_and_belongs_to_many :users
+
+  def populate_by_phrase_key(phrase_key)
+    ActiveRecord::Base.transaction do
+      Phrase.where(key: phrase_key).each do |phrase|
+        phrase.categories << self
+      end
+    end
+  end
 end
