5f321a524743a401d54ebc6d6f0e35b15b8a7958
Capt. Downer <captdowner@gmail.com>
2015-03-13 07:28:37 -0700

    Added methods to user validation and signin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,53 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 #  before_action :set_locale
   include LocaleDetection
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 
   private
+
+  def require_signin
+    unless current_user
+      binding.pry
+      session[:intended_url] = request.url
+      redirect_to new_session_url, alert: "Please sign in!"
+    end
+  end
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  helper_method :current_user?
+
+  def current_user_admin?  
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
+  def current_user_or_admin?
+    current_user || current_user.admin?
+  end
+
+  helper_method :current_user_or_admin?
 
   # extract locale fom tld name
   def extract_locale_from_subdomain
     parsed_locale = ((request.original_url.split /\./)[0].split /\/\//)[1].to_sym
-#    binding.pry
     I18n.available_locales.include?(parsed_locale) ? parsed_locale : nil
   end
 
