7fea99a7aa0df546acc071fc08c68830ffcbeecd
Capt. Downer <captdowner@gmail.com>
2015-03-18 13:06:35 -0700

    Added set_default_role method to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,34 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
 
-  ROLES = {
-    user: 0,
-    vip: 1,
-    admin: 2
-  }
-
-# Save these 2 lines for later development
-#  enum  mode: [:speak, :paste]
-#  enum voice: [:male, :female, :child, :cartoon]
+  def set_default_role
+    self.role ||= :user
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+###
+#  These *may* not be needed
+###
 
-  has_and_belongs_to_many :categories
-  # Do I need this? as the user's language
-  # is taken care of by the TLD name
-  # what does this buy us?
-  belongs_to :language
-
-  include ::Ownership
-
-  def is_admin?
-    self.role == ROLES[:admin]
-  end
-
-  def is_vip?
-    self.role == ROLES[:vip]
-  end
-
-  def is_admin!
-    self.update_attributes(role: ROLES[:admin])
-  end
-
-  def is_vip!
-    self.update_attributes(role: ROLES[:vip])
-  end
-
+###  include ::Ownership
+###
+###  def is_admin?
+###    self.role == ROLES[:admin]
+###  end
+###
+###  def is_vip?
+###    self.role == ROLES[:vip]
+###  end
+###
+###  def is_admin!
+###    self.update_attributes(role: ROLES[:admin])
+###  end
+###
+###  def is_vip!
+###    self.update_attributes(role: ROLES[:vip])
+###  end
 end
