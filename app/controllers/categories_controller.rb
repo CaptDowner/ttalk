30dc2ec965d6a52ebcdc3a529fa349b1aa8a4377
Capt. Downer <captdowner@gmail.com>
2015-03-28 16:46:34 -0700

    Added index and show methods to Categories controller.'

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,10 @@
 class CategoriesController < ApplicationController
-  before_filter: :authenticate_user!
+  before_filter :authenticate_user!
+
+  def index
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 end
