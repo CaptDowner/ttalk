34ea787ee639c06d08f0d3b1e2c1da5edf695b76
Capt. Downer <captdowner@gmail.com>
2015-03-28 16:49:45 -0700

    ommented out authentication methods covered by Devise.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,12 @@
 module SessionsHelper
 
-  def current_user=(user)
-    binding.pry
-    @current_user = user
-  end
+  #def current_user=(user)
+  #  binding.pry
+  #  @current_user = user
+  #end
 
-  def signed_in?
-    !current_user.nil?
-  end
+  #def signed_in?
+  #  !current_user.nil?
+  #end
 
 end
