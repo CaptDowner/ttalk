e3eac838c8edf2186a0447cf0d15c32677b6347b
Capt. Downer <captdowner@gmail.com>
2015-03-28 16:49:14 -0700

    Added index method to populate categories.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,9 @@
 class VisitorsController < ApplicationController
+
+  def index
+    # @categories = []
+    #    @categories = current_user.categories if user_signed_in?
+    @categories ||= Category.all
+  end
+
 end
