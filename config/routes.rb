ded7c80ac70be6319e80ee0ad7262e6187dc8280
Capt. Downer <captdowner@gmail.com>
2015-03-28 17:05:00 -0700

    Added route for credits.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
-  resources :phrases
   resources :category_phrases
+  resources :phrases
   resources :categories
 #  get "signup" => "users#new", :as => "signup"
   get 'phrases/edit'
@@ -11,13 +11,14 @@ Rails.application.routes.draw do
   get 'phrases/new'
   get 'phrases/create'
   get '/about', to: 'high_voltage/pages#show', id: 'about'
+  get '/credits', to: 'high_voltage/pages#credits', id: 'credits'
   get '/faq', to: 'high_voltage/pages#show', id: 'faq'
-  get '/oftenused', to:  'high_voltage/pages#show', id: 'oftenused'
+  get '/oftenused', to: 'high_voltage/pages#show', id: 'oftenused'
   get '/are', to: 'high_voltage/pages#show', id: 'are'
   get '/can', to: 'high_voltage/pages#show', id: 'can-i-you'
   get '/do-you', to: 'high_voltage/pages#show', id: 'do_you'
   get '/how', to: 'high_voltage/pages#show', id: 'how'
-  get '/directions', to:  'high_voltage/pages#show', id: 'directions'
+  get '/directions', to: 'high_voltage/pages#show', id: 'directions'
   get '/i', to: 'high_voltage/pages#show', id: 'i'
   get '/what', to: 'high_voltage/pages#show', id: 'what'
   get '/wwwww', to: 'high_voltage/pages#show', id: 'wwwww'
