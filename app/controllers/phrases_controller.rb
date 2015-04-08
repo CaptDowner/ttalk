235c445c6b3775924b4d4c0151f2c8963e407b88
Capt. Downer <captdowner@gmail.com>
2015-04-07 20:23:05 -0700

    Added new, edit and update methods to controller.

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,6 +1,27 @@
 class PhrasesController < ApplicationController
   def index
-    @phrases = Phrase.all
+    # l gets assigned the language_id of the current_user
+    l = Language.where(:abbrv => I18n.locale.to_s).first.id
+    @phrases = (Phrase.where(language_id: l)).order("phrase_text").page(params[:page]).per(18)
+  end
+  
+  def new
+    @phrase=Phrase.new
+  end
+
+  def edit
+#    requires_admin
+    @phrase = Phrase.find(params[:id])
+  end
+
+  def update
+#    requires_admin
+    @phrase = Phrase.find(params[:id])
+    if @phrase.update(phrase_params)
+      redirect_to @phrase, notice: "Phrase successfully updated!"
+    else
+      render :edit
+    end
   end
 
   def show
